187c8382a9f33f0684d83f23a64cfaa734a2aa0a
shuaibasghar <shuaibasghargtk@gmail.com>
2024-05-27 13:00:28 +0500

    splitting code in to segments

---
+++ 2-react-essentials-deep-dive/notes.js
@@ -0,0 +1,12 @@
+// Fragments: alway use fragment to wrap the component instead of div---because this div is an unnecessary element
+// ---short cut for fragment is   <> </> or <React.Fragment> </React.Fragment>
+
+// when you should split your code into components?
+// 1. Reusability
+// 2. Readability
+// 3. Maintainability
+// 4. Scalability
+// 5. Performance
+// 6. Separation of concerns
+
+// see in app.js ----convert the all the code Section element in spearate component
--- 2-react-essentials-deep-dive/src/App.jsx
+++ 2-react-essentials-deep-dive/src/App.jsx
@@ -1,80 +1,84 @@
-import { useState } from 'react';
-
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data.js';
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import Examples from "./Examples.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+// import CoreConcept from "./components/CoreConcept.jsx";
+// import TabButton from "./components/TabButton.jsx";
+// import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
+  // const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
+  // function handleSelect(selectedButton) {
+  //   // selectedButton => 'components', 'jsx', 'props', 'state'
+  //   setSelectedTopic(selectedButton);
+  //   // console.log(selectedTopic);
+  // }
 
-  console.log('APP COMPONENT EXECUTING');
+  console.log("APP COMPONENT EXECUTING");
 
-  let tabContent = <p>Please select a topic.</p>;
+  // let tabContent = <p>Please select a topic.</p>;
 
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
+  // if (selectedTopic) {
+  //   tabContent = (
+  //     <div id="tab-content">
+  //       <h3>{EXAMPLES[selectedTopic].title}</h3>
+  //       <p>{EXAMPLES[selectedTopic].description}</p>
+  //       <pre>
+  //         <code>{EXAMPLES[selectedTopic].code}</code>
+  //       </pre>
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
+        <CoreConcepts />
+
+        <Examples />
+
+        {/* <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((conceptItem) => (
               <CoreConcept key={conceptItem.title} {...conceptItem} />
             ))}
           </ul>
-        </section>
-        <section id="examples">
+        </section> */}
+        {/* <section id="examples">
           <h2>Examples</h2>
           <menu>
             <TabButton
-              isSelected={selectedTopic === 'components'}
-              onSelect={() => handleSelect('components')}
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleSelect("components")}
             >
               Components
             </TabButton>
             <TabButton
-              isSelected={selectedTopic === 'jsx'}
-              onSelect={() => handleSelect('jsx')}
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleSelect("jsx")}
             >
               JSX
             </TabButton>
             <TabButton
-              isSelected={selectedTopic === 'props'}
-              onSelect={() => handleSelect('props')}
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleSelect("props")}
             >
               Props
             </TabButton>
             <TabButton
-              isSelected={selectedTopic === 'state'}
-              onSelect={() => handleSelect('state')}
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleSelect("state")}
             >
               State
             </TabButton>
           </menu>
           {tabContent}
-        </section>
+        </section> */}
       </main>
-    </div>
+    </>
   );
 }
 
